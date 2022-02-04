4914707d7e6e6f5f8b3208a7e50e7fa36f101cc9
Kotoye Gbemisola <58271499+Barkty@users.noreply.github.com>
2022-02-04 14:27:17 +0100

    update Header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -4,7 +4,17 @@ const Header = () => {
 
     return (
         <div className={styles.header}>
-
+            <div className={styles.header__title}>
+                <h1 className={styles.header__title__name}>Outlook</h1>
+                <p className={styles.header__title__text}>
+                    <span>Connect.</span>
+                    <span>Organize.</span>
+                    <span>Get things done.</span>
+                </p>
+            </div>
+            <div>
+                <p></p>
+            </div>
         </div>
     )
 }
