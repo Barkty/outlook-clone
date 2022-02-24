9b06d62d08230c66e1ca1b6223914b1adffd74d5
Kotoye Gbemisola <58271499+Barkty@users.noreply.github.com>
2022-02-24 17:11:21 +0100

    update products section

--- src/components/products/Products.jsx
+++ src/components/products/Products.jsx
@@ -7,8 +7,45 @@ const Products = () => {
             <div className={styles.productswrap__header}>
                 <h1>
                     <span className={styles.productswrap__header__span}>Access to Office.</span>
-                    <span className={styles.productswrap__header__child}>For free</span>
+                    <span className={styles.productswrap__header__child}>For free.</span>
                 </h1>
+            </div>
+
+            {/* Carousel mini */}
+            <div className={styles.productswrap__carouselMini}>
+                <div className={styles.productswrap__carouselMini__word}>
+                    <div className={styles.productswrap__carouselMini__word__art}></div>
+                    <h1>Word</h1>
+                </div>
+                <div className={styles.productswrap__carouselMini__excel}>
+                    <div className={styles.productswrap__carouselMini__excel__art}></div>
+                    <h1>Excel</h1>
+                </div>
+                <div className={styles.productswrap__carouselMini__ppt}>
+                    <div className={styles.productswrap__carouselMini__ppt__art}></div>
+                    <h1>PowerPoint</h1>
+                </div>
+            </div>
+    
+            {/* Carousel large */}
+            <div className={styles.productswrap__carousel}></div>
+
+            {/* Article */}
+            <article className={styles.productswrap__article}>
+                <p>Collaborating is easy with <span>Word</span>, <span>PowerPoint</span> and <span>Excel</span>. You can chat in real time with <span>Skype-</span> right from your inbox.</p>
+            </article>
+
+            <div className={styles.productswrap__appIcons}>
+                <div className={styles.productswrap__appIcons__outlook}>
+                    <div className={styles.productswrap__appIcons__outlook__spinner}>
+                        <i className={styles.productswrap__appIcons__outlook__spinner__word}></i>
+                        <i className={styles.productswrap__appIcons__outlook__spinner__ppt}></i>
+                        <i className={styles.productswrap__appIcons__outlook__spinner__onedrive}></i>
+                        <i className={styles.productswrap__appIcons__outlook__spinner__onenote}></i>
+                        <i className={styles.productswrap__appIcons__outlook__spinner__skype}></i>
+                        <i className={styles.productswrap__appIcons__outlook__spinner__excel}></i>
+                    </div>
+                </div>
             </div>
         </div>
     )
