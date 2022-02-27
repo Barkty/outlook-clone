7ecf42465b399c593bab9eff66451da4e7211258
Kotoye Gbemisola <58271499+Barkty@users.noreply.github.com>
2022-02-27 16:49:18 +0100

    update scenario

--- src/components/scenerios/Scenerio.jsx
+++ src/components/scenerios/Scenerio.jsx
@@ -2,6 +2,9 @@ import styles from './Scenerio.module.css';
 import androidOne from '../../images/androidOne.png';
 import androidTwo from '../../images/androidTwo.png';
 import androidThree from '../../images/androidThree.png';
+import android1 from '../../images/android1.png';
+import android2 from '../../images/android2.png';
+import android3 from '../../images/android3.png';
 
 const Scenerio = () => {
 
@@ -27,7 +30,15 @@ const Scenerio = () => {
             </div>
             <div className={styles.scenerio__flights}>
                 <div className={styles.scenerio__flights__events}>
-
+                    <div className={styles.scenerio__events__media__singledevice}>
+                        <img src={android1} alt='Android' className={styles.scenerio__events__media__singledevice__img}/>
+                    </div>
+                    <div className={styles.scenerio__events__media__singledevice}>
+                        <img src={android2} alt='Android' className={styles.scenerio__events__media__singledevice__img}/>
+                    </div>
+                    <div className={styles.scenerio__events__media__singledevice}>
+                        <img src={android3} alt='Android' className={styles.scenerio__events__media__singledevice__img}/>
+                    </div>
                 </div>
                 <div className={styles.scenerio__events__caption}>
                     <p className={styles.scenerio__events__caption__text}>
