a556b87d81dc6ad10e511bcfa4a5cb76b92a23a3
Kotoye Gbemisola <58271499+Barkty@users.noreply.github.com>
2022-02-28 16:37:53 +0100

    update create account page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import { Routes, Route } from 'react-router-dom'
 import Home from './pages/home/Home';
+import SignUp from './pages/create/SignUp';
 
 function App() {
   return (
     <div className="App mx-auto">
       <Routes>
         <Route path='/' element={<Home/>}/>
+        <Route path='/signup' element={<SignUp/>}/>
       </Routes>
     </div>
   );
