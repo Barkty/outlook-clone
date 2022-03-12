92a6aba9b0b94044e4bef163f8913d1984c009c1
Kotoye Gbemisola <58271499+Barkty@users.noreply.github.com>
2022-03-12 13:04:41 +0100

    signin update

--- src/pages/signin/SignIn.jsx
+++ src/pages/signin/SignIn.jsx
@@ -8,26 +8,28 @@ const SignIn = () => {
 
     return (
         <div className={styles.signin}>
-            <div className={styles.signin__logo}>
-                <img src={logo} alt="Microsoft logo" className={styles.signin__logo__img}/>
-            </div>
-            <h1 className={styles.signin__title}>Sign in</h1>
-            <div className={styles.signin__form}>
-                <form>
-                    <input
-                    type='text'
-                    placeholder='Email, phone, Skype'/>
-                    <p>No account? <Link to='/signup'>Create one!</Link></p>
-                    <p><Link to='/'>Sign in with a security key</Link><AiOutlineQuestionCircle className={styles.signin__form__icon}/></p>
-                    <button type='submit'>Next</button>
-                </form>
-            </div>
-            <div className={styles.signin__options}>
-                <p><Link to='/'><GrKey className={styles.signin__options__icon}/> Sign-in options</Link></p>
-            </div>
-            <div className={styles.signin__links}>
-                <Link to='/'>Terms of Use</Link>
-                <Link to='/'>Privacy &amp; Cookies</Link>
+            <div className={styles.signin__wrap}>
+                <div className={styles.signin__logo}>
+                    <img src={logo} alt="Microsoft logo" className={styles.signin__logo__img}/>
+                </div>
+                <h1 className={styles.signin__title}>Sign in</h1>
+                <div className={styles.signin__form}>
+                    <form>
+                        <input
+                        type='text'
+                        placeholder='Email, phone, Skype'/>
+                        <p>No account? <Link to='/signup'>Create one!</Link></p>
+                        <p><Link to='/'>Sign in with a security key</Link><AiOutlineQuestionCircle className={styles.signin__form__icon}/></p>
+                        <button type='submit'>Next</button>
+                    </form>
+                </div>
+                <div className={styles.signin__options}>
+                    <p><Link to='/'><GrKey className={styles.signin__options__icon}/> Sign-in options</Link></p>
+                </div>
+                <div className={styles.signin__links}>
+                    <Link to='/'>Terms of Use</Link>
+                    <Link to='/'>Privacy &amp; Cookies</Link>
+                </div>
             </div>
         </div>
     )
