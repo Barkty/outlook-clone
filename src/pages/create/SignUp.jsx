429ae60827a8cf8ee4d7db7b5ead49f561b92460
Kotoye Gbemisola <58271499+Barkty@users.noreply.github.com>
2022-03-14 12:34:06 +0100

    responsive signup

--- src/pages/create/SignUp.jsx
+++ src/pages/create/SignUp.jsx
@@ -22,38 +22,40 @@ const SignUp = () => {
 
     return (
         <div className={styles.signup}>
-            <div className={styles.signup__logo}>
-                <img src={logo} alt="Microsoft logo" className={styles.signup__logo__img}/>
-            </div>
-            <h1 className={styles.signup__title}>Create account</h1>
-            <div className={styles.signup__form}>
-                <form onSubmit={formik.handleSubmit}>
-                    <div className={styles.signup__form__field}>
-                        <input 
-                        type='text'
-                        id='email'
-                        name='email'
-                        onChange={formik.handleChange}
-                        value={formik.values.email}
-                        placeholder='New email'/>
-                        <select title='Listed emails'
-                            name='domain' 
-                            id='domain' 
-                            onChange={formik.handleChange} 
-                            value={formik.values.domain}>
-                            <option value='@outlook.com'>@outlook.com</option>
-                            <option value='@hotmail.com'>@hotmail.com</option>
-                        </select>
-                    </div>
-                    {formik.errors.email && formik.touched.email ? (
-                        <span className={styles.signup__form__field__error}>{formik.errors.email}</span>
-                    ) : null}
-                    <button type='submit'>Next</button>
-                </form>
-            </div>
-            <div className={styles.signup__links}>
-                <Link to='/'>Terms of Use</Link>
-                <Link to='/'>Privacy &amp; Cookies</Link>
+            <div className={styles.signup__wrap}>
+                <div className={styles.signup__logo}>
+                    <img src={logo} alt="Microsoft logo" className={styles.signup__logo__img}/>
+                </div>
+                <h1 className={styles.signup__title}>Create account</h1>
+                <div className={styles.signup__form}>
+                    <form onSubmit={formik.handleSubmit}>
+                        <div className={styles.signup__form__field}>
+                            <input 
+                            type='text'
+                            id='email'
+                            name='email'
+                            onChange={formik.handleChange}
+                            value={formik.values.email}
+                            placeholder='New email'/>
+                            <select title='Listed emails'
+                                name='domain' 
+                                id='domain' 
+                                onChange={formik.handleChange} 
+                                value={formik.values.domain}>
+                                <option value='@outlook.com'>@outlook.com</option>
+                                <option value='@hotmail.com'>@hotmail.com</option>
+                            </select>
+                        </div>
+                        {formik.errors.email && formik.touched.email ? (
+                            <span className={styles.signup__form__field__error}>{formik.errors.email}</span>
+                        ) : null}
+                        <button type='submit'>Next</button>
+                    </form>
+                </div>
+                <div className={styles.signup__links}>
+                    <Link to='/'>Terms of Use</Link>
+                    <Link to='/'>Privacy &amp; Cookies</Link>
+                </div>
             </div>
         </div>
     )
