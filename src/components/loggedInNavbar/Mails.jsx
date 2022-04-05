a7537392bc13290ba2e23b2f190fc09c83ddddb2
Gbemisola Kotoye <barkty@yahoo.com>
2022-04-05 14:09:08 +0100

    update mail

--- src/components/loggedInNavbar/Mails.jsx
+++ src/components/loggedInNavbar/Mails.jsx
@@ -1,10 +1,21 @@
 import styles from './Navbar.module.css'
-
+import { CgCheckO, CgChevronDown } from 'react-icons/cg'
 const Mail = () => {
 
     return (
         <div className={styles.mail}>
-
+            <div className={styles.mail__nav}>
+                <CgCheckO/>
+                <div className={styles.mail__nav__focus}>
+                    Focused
+                </div>
+                <div className={styles.mail__nav__focus}>
+                    Other
+                </div>
+                <div className={styles.mail__nav__filter}>
+                    Filter <CgChevronDown/>
+                </div>
+            </div>
         </div>
     )
 }
