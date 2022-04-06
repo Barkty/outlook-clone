ed17fad47178c19d9ae06775b2d332abef1bdcea
Gbemisola Kotoye <barkty@yahoo.com>
2022-04-06 17:27:10 +0100

    message

--- src/components/loggedInNavbar/Mails.jsx
+++ src/components/loggedInNavbar/Mails.jsx
@@ -8,19 +8,30 @@ const Mail = () => {
         <div className={styles.mail}>
             <div className={styles.mail__nav}>
                 <CgCheckO className={styles.mail__nav__icon}/>
-                <div className={active === 'Focus' ? styles.mail__nav__focus__active : styles.mail__nav__focus} onClick={()=>setActive('Focus')}>
+                <div className={active === 'Focus' ? styles.mail__nav__active : styles.mail__nav__focus} onClick={()=>setActive('Focus')}>
                     Focused
                 </div>
-                <div className={active === 'Other' ? styles.mail__nav__focus__active : styles.mail__nav__focus} onClick={()=>setActive('Other')}>
+                <div className={active === 'Other' ? styles.mail__nav__active : styles.mail__nav__focus} onClick={()=>setActive('Other')}>
                     Other
                 </div>
                 <div className={styles.mail__nav__filter}>
                     Filter <CgChevronDown/>
                 </div>
             </div>
-            <div className={styles.mail__nav}>
-                <div className={styles.mail__nav__user}>
-
+            <div className={styles.mail__user}>
+                <div className={styles.mail__user__icon}></div>
+                <div className={styles.mail__user__mess}>
+                <p>Microsoft Account Team</p>
+                    <p>New apps connected to your....</p>
+                    <p>Microsoft account New app(s) connected to your ....</p>
+                </div>
+            </div>
+            <div className={styles.mail__user}>
+                <div className={styles.mail__user__icon}></div>
+                <div className={styles.mail__user__mess}>
+                    <p>Microsoft Account Team</p>
+                    <p>New apps connected to your....</p>
+                    <p>Microsoft account New app(s) connected to your ....</p>
                 </div>
             </div>
         </div>
