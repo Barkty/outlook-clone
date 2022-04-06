b86cf0489ebdc5321a920ba6ec43d760a730ea2b
Gbemisola Kotoye <barkty@yahoo.com>
2022-04-06 16:46:47 +0100

    mails

--- src/components/loggedInNavbar/Mails.jsx
+++ src/components/loggedInNavbar/Mails.jsx
@@ -2,20 +2,25 @@ import styles from './Navbar.module.css'
 import { CgCheckO, CgChevronDown } from 'react-icons/cg'
 import { useState } from 'react'
 const Mail = () => {
-    const [active, setActive] = useState(true);
+    const [active, setActive] = useState('Focus');
     
     return (
         <div className={styles.mail}>
             <div className={styles.mail__nav}>
                 <CgCheckO className={styles.mail__nav__icon}/>
-                <div className={styles.mail__nav__focus}>
+                <div className={active === 'Focus' ? styles.mail__nav__focus__active : styles.mail__nav__focus} onClick={()=>setActive('Focus')}>
                     Focused
                 </div>
-                <div className={styles.mail__nav__focus}>
+                <div className={active === 'Other' ? styles.mail__nav__focus__active : styles.mail__nav__focus} onClick={()=>setActive('Other')}>
                     Other
                 </div>
                 <div className={styles.mail__nav__filter}>
                     Filter <CgChevronDown/>
+                </div>
+            </div>
+            <div className={styles.mail__nav}>
+                <div className={styles.mail__nav__user}>
+
                 </div>
             </div>
         </div>
